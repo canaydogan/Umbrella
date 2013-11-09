b921dbd727bbb65c2086bc34508ee46c9e5e1280
Can Aydoğan <canaydogan89@gmail.com>
2013-11-09 23:53:46 +0200

    Added methods

--- src/main/java/net/canaydogan/umbrella/request/HttpRequest.java
+++ src/main/java/net/canaydogan/umbrella/request/HttpRequest.java
@@ -2,8 +2,16 @@ package net.canaydogan.umbrella.request;
 
 public interface HttpRequest {
 	
+	public enum Method {
+		GET, POST, PUT, DELETE
+	}
+	
 	public String getUri();
 	
 	public HttpRequest setUri(String uri);
+	
+	public Method getMethod();
+	
+	public HttpRequest setMethod(Method method);
 
 }

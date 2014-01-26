8a6620a32d8afd79099d37a0ee464abf13243550
Can Aydoğan <canaydogan89@gmail.com>
2014-01-26 15:46:52 +0200

    Added InternalServerError

--- src/main/java/net/canaydogan/umbrella/HttpResponse.java
+++ src/main/java/net/canaydogan/umbrella/HttpResponse.java
@@ -4,7 +4,8 @@ public interface HttpResponse {
 
 	public enum Status {
 		OK(200), CONTINUE(100), NOT_MODIFIED(304), NOT_FOUND(404), 
-		FORBIDDEN(403), METHOD_NOT_ALLOWED(405), BAD_REQUEST(400);
+		FORBIDDEN(403), METHOD_NOT_ALLOWED(405), BAD_REQUEST(400),
+		INTERNAL_SERVER_ERROR(500);
 		
 		private final int code;
 		
